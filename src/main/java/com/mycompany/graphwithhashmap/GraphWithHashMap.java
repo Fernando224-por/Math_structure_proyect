695b4a00a024090faa9507afb0973d64c95711fd
Fernando224-por <andresfernandoxd1591@gmail.com>
2025-04-15 12:29:18 -0500

    feat(pom): create graph reading Json archive

--- src/main/java/com/mycompany/graphwithhashmap/GraphWithHashMap.java
+++ src/main/java/com/mycompany/graphwithhashmap/GraphWithHashMap.java
@@ -9,18 +9,35 @@ package com.mycompany.graphwithhashmap;
  * @author andfe
  */
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.io.File;
+import java.io.IOException;
+
 public class GraphWithHashMap {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
         simpleGraph g = new simpleGraph();
-        g.addEdges("A", "B", 4);
-        g.addEdges("A", "C", 2);
-        g.addEdges("B", "E", 3);
-        g.addEdges("C", "D", 2);
-        g.addEdges("C", "F", 4);
-        g.addEdges("D", "F", 1);
-        g.addEdges("E", "G", 2);
-        g.addEdges("F", "G", 1);
+        
+        File myFile = new File("testFile.json");
+        
+        if (myFile.exists()) {
+            JsonNode readJson = objectMapper.readTree(new File("testFile.json"));
+            JsonNode ubicaciones = readJson.get("Puntos");
+            for (JsonNode ubicacion : ubicaciones) {
+                String from = ubicacion.get("from").asText();
+                String to = ubicacion.get("to").asText();
+                int weight = ubicacion.get("weight").asInt();
+                
+                g.addEdges(from, to, weight);
+                
+                System.out.println("Los nodos " + from + " y " + to + " han sido añadidos");
+                
+            }
+        } else {
+            System.out.println("El archivo no existe");
+        }
         
         simpleGraph.PathResult resultTestOne = g.findShortestPath("A", "G");
         System.out.println(resultTestOne);
